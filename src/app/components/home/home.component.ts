4bd9fcbbb96551bb3dd567474cb9decc45565c98
zachalexander <alexander.d.zachary@gmail.com>
2020-01-11 20:06:46 -0500

    updates responsive design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faMedium} from '@fortawesome/free-brands-svg-icons';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -11,14 +8,39 @@ import { faMedium} from '@fortawesome/free-brands-svg-icons';
 })
 export class HomeComponent implements OnInit {
 
-  faTwitter = faTwitter;
-  faInstagram = faInstagram;
-  faMedium = faMedium;
-  faEnvelope = faEnvelope;
+  mobile = false;
 
   constructor() { }
-
+  
   ngOnInit() {
+    if(window.innerWidth <= 767) {
+      this.mobile = true;
+      console.log(this.mobile);
+    }
+  }
+  
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll(e) {
+    const element = document.querySelector('.nav-wrapper');
+    const orangeDiv = document.querySelector('.mid2-left');
+    const topOfOrangeDiv = orangeDiv.getBoundingClientRect()['y'];
+
+    if (window.innerWidth >= 767) {
+      if (topOfOrangeDiv < 70 && topOfOrangeDiv > -925) {
+        element.classList.add('nav-wrapper-light');
+      } else {
+        element.classList.remove('nav-wrapper-light');
+      }
+    } else {
+      if (topOfOrangeDiv < 70 && topOfOrangeDiv > -325) {
+        element.classList.add('nav-wrapper-light');
+      } else {
+        element.classList.remove('nav-wrapper-light');
+      }
+    }
+
+
   }
 
 }
